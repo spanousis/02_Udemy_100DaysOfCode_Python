ae0b56f012575b1abb2315788eab3413c7cd3429
spanousis <sspanousis@gmail.com>
2024-04-23 20:46:28 +0300

    first code (first try)

---
+++ Code1.py
@@ -0,0 +1,9 @@
+two_digit_number = input("Give a 2 digit number: ")
+
+first_digit = int(two_digit_number[0])
+second_digit = int(two_digit_number[1])
+
+two_digit_number = str(first_digit) + str(second_digit)
+
+print(f"first digit is {first_digit} and the second digit is {second_digit}")
+print(f"two digit number is {two_digit_number}")
