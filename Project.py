2ee9cb4f4d127f0ece5a4173dde6490863366fc1
spanousis <sspanousis@gmail.com>
2024-05-03 03:55:23 +0300

    Fix syntax of Final Project

--- Project.py
+++ Project.py
@@ -3,7 +3,6 @@ totalBill = input("What was the total bill? $")
 tipPercentage = input("How much tip would you like to give? 10, 12, or 15? ")
 numberOfPeople = input("How many people to split the bill? ")
 
-result = float(totalBill) * (1 + int(tipPercentage) / 100) / \
-    int(numberOfPeople)
+result = float(totalBill) * (1 + int(tipPercentage) / 100) / int(numberOfPeople)
 
 print(f"Each person should pay: ${round(result, 2)}")
